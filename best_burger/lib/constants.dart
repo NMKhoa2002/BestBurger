38d9166862715d8e639f37fa8bdc1d9b9958ee71
NguyenMinhKhoa <nguyenkhoa.thitran2.96@gmail.com>
2024-03-15 13:27:56 +0700

    khoa

--- best_burger/lib/constants.dart
+++ best_burger/lib/constants.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 
-const kTextColor = Color(0xFF535353);
+const kTextColor = Color.fromARGB(255, 46, 46, 46);
 const kTextLightColor = Color(0xFFACACAC);
 const textColor = Color.fromRGBO(243, 86, 33, 0.992);
 const kDefaultPadding = 20.0;
+const Color primary = Color(0xFF1FCC79);
+const Color Secondary = Color(0xFFFF6464);
+const Color mainText = Color(0xFF2E3E5C);
+const Color SecondaryText = Color(0xFF9FA5C0);
+const Color outline = Color(0xFFD0DBEA);
+const Color form = Color(0xFFF4F5F7);
